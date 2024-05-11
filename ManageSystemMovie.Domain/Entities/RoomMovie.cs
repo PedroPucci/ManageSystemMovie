ff817181ccd407c33777387c44a73085fe8bac3c
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-05-11 14:30:55 -0300

    fix: correção uma RoomMovie pode ter vários Movies

--- ManageSystemMovie.Domain/Entities/RoomMovie.cs
+++ ManageSystemMovie.Domain/Entities/RoomMovie.cs
@@ -5,6 +5,7 @@ namespace ManageSystemMovie.Domain.Entities
     public class RoomMovie : BaseEntity
     {
         public int Number { get; set; }
-        public string? Description { get; set; }        
+        public string? Description { get; set; }
+        public List<Movie> Movies { get; set; }
     }
 }
