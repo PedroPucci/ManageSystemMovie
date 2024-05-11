43aba733bcedf9bdf7ce6023b590a04d89bc5d24
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-05-11 14:17:38 -0300

    feat: criação das entidades e da classe BaseEntity

---
+++ ManageSystemMovie.Domain/Entities/Movie.cs
@@ -0,0 +1,11 @@
+﻿using ManageSystemMovie.Domain.General;
+
+namespace ManageSystemMovie.Domain.Entities
+{
+    public class Movie : BaseEntity
+    {        
+        public string? Name { get; set; }
+        public string? Director { get; set; }
+        public int TimeMovie { get; set; }
+    }
+}
---
+++ ManageSystemMovie.Domain/Entities/RoomMovie.cs
@@ -0,0 +1,10 @@
+﻿using ManageSystemMovie.Domain.General;
+
+namespace ManageSystemMovie.Domain.Entities
+{
+    public class RoomMovie : BaseEntity
+    {
+        public int Number { get; set; }
+        public string? Description { get; set; }        
+    }
+}
