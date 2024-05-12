6e7911f3b1278576470485fe46ece08bce316773
pedro.h.pucci <pedro.pucci@mutant.com.br>
2024-05-12 12:42:18 -0300

    feat: criacao dos testes unitarios para as duas entidades Movie e RoomMovie

---
+++ ManageSystemMovie.Test/Entities/MovieTest.cs
@@ -0,0 +1,29 @@
+﻿using ManageSystemMovie.Domain.Entities;
+
+namespace ManageSystemMovie.Test.Entities
+{
+    public class MovieTest
+    {
+        [Fact]
+        public void Movie_Success_ShouldHaveCorrectProperties()
+        {
+            // Arrange
+            string expectedName = "Guerra dos mundos";
+            string expectedDirector = "Pedro Ighor Holanda";
+            int expectedTimeMovie = 200;
+
+            // Act
+            var movie = new Movie
+            {
+                Name = expectedName,
+                Director = expectedDirector,
+                TimeMovie = expectedTimeMovie
+            };
+
+            // Assert
+            Assert.Equal(expectedName, movie.Name);
+            Assert.Equal(expectedDirector, movie.Director);
+            Assert.Equal(expectedTimeMovie, movie.TimeMovie);
+        }
+    }
+}
---
+++ ManageSystemMovie.Test/Entities/RoomMovieTest.cs
@@ -0,0 +1,33 @@
+﻿using ManageSystemMovie.Domain.Entities;
+
+namespace ManageSystemMovie.Test.Entities
+{
+    public class RoomMovieTest
+    {
+        [Fact]
+        public void RoomMovie_WhenInitialized_ShouldHaveCorrectProperties()
+        {
+            // Arrange
+            int expectedNumber = 1;
+            string expectedDescription = "Description 1";
+            var expectedMovies = new List<Movie>
+            {
+                new Movie { Name = "The Gladiator Redemption", Director = "Francis Ford Coppola", TimeMovie = 192 },
+                new Movie { Name = "The Godfather The Final History", Director = "Francis Ford Coppola", TimeMovie = 105 }
+            };
+
+            // Act
+            var roomMovie = new RoomMovie
+            {
+                Number = expectedNumber,
+                Description = expectedDescription,
+                Movies = expectedMovies
+            };
+
+            // Assert
+            Assert.Equal(expectedNumber, roomMovie.Number);
+            Assert.Equal(expectedDescription, roomMovie.Description);
+            Assert.Equal(expectedMovies, roomMovie.Movies);
+        }
+    }
+}
